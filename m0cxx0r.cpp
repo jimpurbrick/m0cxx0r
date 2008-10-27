b98052ecd0cf990fceaa49c3bced3f592e164f35
dave.hillier1 <dave.hillier1@648d92d8-a3b6-11dd-8805-4530818397e5>
2008-10-27 18:11:22 +0000

    * Fixed indentation.

--- m0cxx0r.cpp
+++ m0cxx0r.cpp
@@ -33,280 +33,280 @@
 
 namespace m0cxx0r
 {
-  class IParam
-  {
-  public:
-    IParam() {;}
-    virtual ~IParam() {;}
-    virtual IParam* clone() = 0;
-    virtual void setValue(unsigned char* p0) = 0;
-    virtual bool isEqual(IParam* iparam) = 0;
-    virtual std::string getString() = 0;
-  };
-
-  template<typename ParamType>
-  struct Param : public IParam
-  {
-  public:
-
-    Param(unsigned char* p0, ParamType* param) :
-      mOffset((unsigned char*)(param) - p0),
-      mParam(*param)
+    class IParam
     {
-    }
+    public:
+        IParam() {;}
+        virtual ~IParam() {;}
+        virtual IParam* clone() = 0;
+        virtual void setValue(unsigned char* p0) = 0;
+        virtual bool isEqual(IParam* iparam) = 0;
+        virtual std::string getString() = 0;
+    };
 
-    virtual ~Param() {;}
-
-    virtual IParam* clone()
+    template<typename ParamType>
+    struct Param : public IParam
     {
-      Param* result = new Param();
-      result->mOffset = mOffset;
-      result->mParam = mParam;
-      return result;
-    }
+    public:
 
-    virtual void setValue(unsigned char* p0)
+        Param(unsigned char* p0, ParamType* param) :
+          mOffset((unsigned char*)(param) - p0),
+              mParam(*param)
+          {
+          }
+
+          virtual ~Param() {;}
+
+          virtual IParam* clone()
+          {
+              Param* result = new Param();
+              result->mOffset = mOffset;
+              result->mParam = mParam;
+              return result;
+          }
+
+          virtual void setValue(unsigned char* p0)
+          {
+              ParamType* param = (ParamType*)(p0 + mOffset);
+              mParam = *param;
+          }
+
+          virtual bool isEqual(IParam* iparam)
+          {
+              Param* param = static_cast<Param*>(iparam);
+              return mParam == param->mParam;
+          }
+
+          virtual std::string getString()
+          {
+              std::ostringstream stream;
+              stream << mParam;
+              return stream.str();
+          }
+
+    private:
+
+        Param() {;}
+
+        int mOffset;
+        ParamType mParam;
+    };
+
+    class Call
     {
-      ParamType* param = (ParamType*)(p0 + mOffset);
-      mParam = *param;
-    }
+    public:
+        Call(const std::string& name) :
+          mName(name)
+          {
+          }
 
-    virtual bool isEqual(IParam* iparam)
+          virtual ~Call() {;}
+
+          virtual Call* clone()
+          {
+              Call* result = new Call(mName);
+              result->mIndex = mIndex;
+              std::vector<IParam*>::iterator i;
+              for(i = mParameters.begin(); i != mParameters.end(); ++i)
+              {
+                  result->addParameter((*i)->clone());
+              }
+              return result;
+          }
+
+          void addParameter(IParam* param) {mParameters.push_back(param);}
+
+          size_t getIndex()
+          {
+              return mIndex;
+          }
+
+          std::string getName()
+          {
+              return mName;
+          }
+
+          void setIndex(size_t index)
+          {
+              mIndex = index;
+          }
+
+          void setParams(unsigned char* params)
+          {
+              ParamVector::iterator i;
+              for(i = mParameters.begin(); i != mParameters.end(); ++i)
+              {
+                  (*i)->setValue(params);
+              }
+          }
+
+          bool verify(Call* actual)
+          {
+              bool result = true;
+              size_t index = 0;
+              ParamVector::const_iterator expectedIter = mParameters.begin();
+              ParamVector::const_iterator actualIter = actual->mParameters.begin();
+              while(expectedIter != mParameters.end())
+              {
+                  if(! (*expectedIter)->isEqual(*actualIter))
+                  {
+                      result = false;
+                      std::cerr << "Failure! m0cxx0r"
+                          << " expected " << (*expectedIter)->getString()
+                          << " as parameter " << index
+                          << " of " << getName() << " call"
+                          << " (actual " << (*actualIter)->getString() << ')'
+                          << std::endl;
+                  }
+                  ++index;
+                  ++expectedIter;
+                  ++actualIter;
+              }
+              return result;
+          }
+
+    private:
+        std::string mName;
+        size_t mIndex;
+        typedef std::vector<IParam*> ParamVector;
+        ParamVector mParameters;
+    };
+
+    template<typename T>
+    class Mock : public T
     {
-      Param* param = static_cast<Param*>(iparam);
-      return mParam == param->mParam;
-    }
+    public:
 
-    virtual std::string getString()
-    {
-      std::ostringstream stream;
-      stream << mParam;
-      return stream.str();
-    }
+        static Mock* create()
+        {
+            // Allocate memory for mock.
+            size_t mockSize = sizeof(Mock<T>);
+            unsigned char* mockData = new unsigned char[mockSize];
+            memset(mockData, 0, mockSize);
 
-  private:
+            // Create VTableDonor object in the memory allocated for the mock to 
+            // point the Mock VPtr at the VTableDonor VTable using placement new.
+            Mock<T>* mock = reinterpret_cast<Mock<T>*>(new (mockData) VTableDonor);
 
-    Param() {;}
+            // Initialize mock data members.
+            mock->mExpectedCalls = std::vector<Call*>();
+            mock->mActualCalls = std::vector<Call*>();
+            mock->mRecordingExpected = false;
 
-    int mOffset;
-    ParamType mParam;
-  };
+            return mock;
+        }
 
-  class Call
-  {
-  public:
-    Call(const std::string& name) :
-      mName(name)
-    {
-    }
+        static void destroy(Mock** mock)
+        {
+            // Delete byte array created for mock.
+            unsigned char* mockData = (unsigned char*) (*mock);
+            delete[] mockData;
 
-    virtual ~Call() {;}
+            // Null given pointer.
+            mock = NULL;
+        }
 
-    virtual Call* clone()
-    {
-      Call* result = new Call(mName);
-      result->mIndex = mIndex;
-      std::vector<IParam*>::iterator i;
-      for(i = mParameters.begin(); i != mParameters.end(); ++i)
-      {
-	result->addParameter((*i)->clone());
-      }
-      return result;
-    }
+        template<typename MemberFuncType>
+        void expect(const std::string& name, MemberFuncType func)
+        {
+            mRecordingExpected = true;
+            Call* expectedCall = new Call(name);
+            mExpectedCalls.push_back(expectedCall);
+            ((this)->*(func))();
+            mRecordingExpected = false;
+        }
 
-    void addParameter(IParam* param) {mParameters.push_back(param);}
+        template<typename MemberFuncType, typename ParamType0>
+        void expect(const std::string& name, MemberFuncType func, ParamType0 p0)
+        {
+            mRecordingExpected = true;
+            Call* expectedCall = new Call(name);
+            expectedCall->addParameter(new Param<ParamType0>((unsigned char*)(&p0), &p0));
+            mExpectedCalls.push_back(expectedCall);
+            ((this)->*(func))(p0);
+            mRecordingExpected = false;
+        }
 
-    size_t getIndex()
-    {
-      return mIndex;
-    }
+        template<typename MemberFuncType, typename ParamType0, typename ParamType1>
+        void expect(const std::string& name, MemberFuncType func, ParamType0 p0, ParamType1 p1)
+        {
+            mRecordingExpected = true;
+            Call* expectedCall = new Call(name);
+            expectedCall->addParameter(new Param<ParamType0>((unsigned char*)(&p0), &p0));
+            expectedCall->addParameter(new Param<ParamType1>((unsigned char*)(&p0), &p1));
+            mExpectedCalls.push_back(expectedCall);
+            ((this)->*(func))(p0, p1);
+            mRecordingExpected = false;
+        }
 
-    std::string getName()
-    {
-      return mName;
-    }
+        bool verify()
+        {
+            bool result = true;
+            CallVector::const_iterator actualIter = mActualCalls.begin();
+            CallVector::const_iterator expectedIter = mExpectedCalls.begin();
+            while(actualIter != mActualCalls.end())
+            {
+                result &= (*expectedIter)->verify(*actualIter);
+                ++actualIter;
+                ++expectedIter;
+            }
+            while(expectedIter != mExpectedCalls.end())
+            {
+                std::cerr << "Failure! m0cxx0r expected "
+                    << (*expectedIter)->getName() << " call" << std::endl;
+                ++expectedIter;
+            }
+            return result;
+        }
 
-    void setIndex(size_t index)
-    {
-      mIndex = index;
-    }
+    private:
 
-    void setParams(unsigned char* params)
-    {
-      ParamVector::iterator i;
-      for(i = mParameters.begin(); i != mParameters.end(); ++i)
-      {
-	(*i)->setValue(params);
-      }
-    }
+        Mock()
+        {
+            std::cerr << "Error! m0cxx0r::Mock objects must not be constructed" 
+                << std::endl;
+        }
 
-    bool verify(Call* actual)
-    {
-      bool result = true;
-      size_t index = 0;
-      ParamVector::const_iterator expectedIter = mParameters.begin();
-      ParamVector::const_iterator actualIter = actual->mParameters.begin();
-      while(expectedIter != mParameters.end())
-      {
-	if(! (*expectedIter)->isEqual(*actualIter))
-	{
-	  result = false;
-	  std::cerr << "Failure! m0cxx0r"
-		    << " expected " << (*expectedIter)->getString()
-	            << " as parameter " << index
-		    << " of " << getName() << " call"
-		    << " (actual " << (*actualIter)->getString() << ')'
-		    << std::endl;
-	}
-	++index;
-	++expectedIter;
-	++actualIter;
-      }
-      return result;
-    }
+        virtual ~Mock()
+        {
+            std::cerr << "Error! m0cxx0r::Mock objects must not be deleted" 
+                << std::endl;
+        }
 
-  private:
-    std::string mName;
-    size_t mIndex;
-    typedef std::vector<IParam*> ParamVector;
-    ParamVector mParameters;
-  };
+        class VTableDonor
+        {
+            virtual void  f1(size_t p0) {((Mock*) this)->recordCall(1, (unsigned char*)(&p0));}
+            virtual void  f2(size_t p0) {((Mock*) this)->recordCall(2, (unsigned char*)(&p0));}
+            virtual void  f3(size_t p0) {((Mock*) this)->recordCall(3, (unsigned char*)(&p0));}
+            // TODO: Add more virtual methods as needed.
+        };
 
-template<typename T>
-class Mock : public T
-{
-public:
-  
-  static Mock* create()
-  {
-    // Allocate memory for mock.
-    size_t mockSize = sizeof(Mock<T>);
-    unsigned char* mockData = new unsigned char[mockSize];
-	memset(mockData, 0, mockSize);
+        void recordCall(size_t index, unsigned char* params)
+        {
+            if(mRecordingExpected)
+            {
+                Call* call = mExpectedCalls.back();
+                call->setIndex(index);
+                call->setParams(params);
+            }
+            else
+            {
+                size_t callIndex = mActualCalls.size();
+                Call* expectedCall = mExpectedCalls[callIndex];
+                if(expectedCall->getIndex() == index)
+                {
+                    Call* actualCall = expectedCall->clone();
+                    actualCall->setParams(params);
+                    mActualCalls.push_back(actualCall);
+                }
+            }
+        }
 
-    // Create VTableDonor object in the memory allocated for the mock to 
-	// point the Mock VPtr at the VTableDonor VTable using placement new.
-	Mock<T>* mock = reinterpret_cast<Mock<T>*>(new (mockData) VTableDonor);
-
-    // Initialize mock data members.
-    mock->mExpectedCalls = std::vector<Call*>();
-    mock->mActualCalls = std::vector<Call*>();
-    mock->mRecordingExpected = false;
-
-    return mock;
-  }
-
-  static void destroy(Mock** mock)
-  {
-    // Delete byte array created for mock.
-    unsigned char* mockData = (unsigned char*) (*mock);
-    delete[] mockData;
-
-    // Null given pointer.
-    mock = NULL;
-  }
-
-  template<typename MemberFuncType>
-  void expect(const std::string& name, MemberFuncType func)
-  {
-    mRecordingExpected = true;
-    Call* expectedCall = new Call(name);
-    mExpectedCalls.push_back(expectedCall);
-    ((this)->*(func))();
-    mRecordingExpected = false;
-  }
-
-  template<typename MemberFuncType, typename ParamType0>
-  void expect(const std::string& name, MemberFuncType func, ParamType0 p0)
-  {
-    mRecordingExpected = true;
-    Call* expectedCall = new Call(name);
-    expectedCall->addParameter(new Param<ParamType0>((unsigned char*)(&p0), &p0));
-    mExpectedCalls.push_back(expectedCall);
-    ((this)->*(func))(p0);
-    mRecordingExpected = false;
-  }
-
-  template<typename MemberFuncType, typename ParamType0, typename ParamType1>
-  void expect(const std::string& name, MemberFuncType func, ParamType0 p0, ParamType1 p1)
-  {
-    mRecordingExpected = true;
-    Call* expectedCall = new Call(name);
-    expectedCall->addParameter(new Param<ParamType0>((unsigned char*)(&p0), &p0));
-	expectedCall->addParameter(new Param<ParamType1>((unsigned char*)(&p0), &p1));
-    mExpectedCalls.push_back(expectedCall);
-    ((this)->*(func))(p0, p1);
-    mRecordingExpected = false;
-  }
-
-  bool verify()
-  {
-    bool result = true;
-    CallVector::const_iterator actualIter = mActualCalls.begin();
-    CallVector::const_iterator expectedIter = mExpectedCalls.begin();
-    while(actualIter != mActualCalls.end())
-    {
-      result &= (*expectedIter)->verify(*actualIter);
-      ++actualIter;
-      ++expectedIter;
-    }
-    while(expectedIter != mExpectedCalls.end())
-    {
-      std::cerr << "Failure! m0cxx0r expected "
-		<< (*expectedIter)->getName() << " call" << std::endl;
-      ++expectedIter;
-    }
-    return result;
-  }
-
-private:
-
-  Mock()
-  {
-    std::cerr << "Error! m0cxx0r::Mock objects must not be constructed" 
-	      << std::endl;
-  }
-
-  virtual ~Mock()
-  {
-    std::cerr << "Error! m0cxx0r::Mock objects must not be deleted" 
-	      << std::endl;
-  }
-
-  class VTableDonor
-  {
-    virtual void  f1(size_t p0) {((Mock*) this)->recordCall(1, (unsigned char*)(&p0));}
-    virtual void  f2(size_t p0) {((Mock*) this)->recordCall(2, (unsigned char*)(&p0));}
-    virtual void  f3(size_t p0) {((Mock*) this)->recordCall(3, (unsigned char*)(&p0));}
-    // TODO: Add more virtual methods as needed.
-  };
-
-  void recordCall(size_t index, unsigned char* params)
-  {
-    if(mRecordingExpected)
-    {
-      Call* call = mExpectedCalls.back();
-      call->setIndex(index);
-      call->setParams(params);
-    }
-    else
-    {
-      size_t callIndex = mActualCalls.size();
-      Call* expectedCall = mExpectedCalls[callIndex];
-      if(expectedCall->getIndex() == index)
-      {
-	Call* actualCall = expectedCall->clone();
-	actualCall->setParams(params);
-	mActualCalls.push_back(actualCall);
-      }
-    }
-  }
-
-  typedef std::vector<Call*> CallVector;
-  CallVector mExpectedCalls;
-  CallVector mActualCalls;
-  bool mRecordingExpected;
-};
+        typedef std::vector<Call*> CallVector;
+        CallVector mExpectedCalls;
+        CallVector mActualCalls;
+        bool mRecordingExpected;
+    };
 
 } // namespace m0cxx0r
 
@@ -314,36 +314,36 @@ class ProductionClass
 {
 public:
 
-  ProductionClass(size_t param)
-  {
-    std::cerr << "Error! ProductionClass ctor should never be called" << std::endl;
-  }
-  
-  virtual void foo(size_t p1) 
-  {
-	std::cerr << "Error! ProductionClass foo should never be called" << std::endl;
-  }
+    ProductionClass(size_t param)
+    {
+        std::cerr << "Error! ProductionClass ctor should never be called" << std::endl;
+    }
 
-  virtual void bar(size_t p1, size_t p2)
-  {
-	std::cerr << "Error! ProductionClass bar should never be called" << std::endl;
-  }
+    virtual void foo(size_t p1) 
+    {
+        std::cerr << "Error! ProductionClass foo should never be called" << std::endl;
+    }
 
-  virtual void baz(); // Deliberately no body to ensure this does not cause a linker error
+    virtual void bar(size_t p1, size_t p2)
+    {
+        std::cerr << "Error! ProductionClass bar should never be called" << std::endl;
+    }
+
+    virtual void baz(); // Deliberately no body to ensure this does not cause a linker error
 
 
 };
 
 int main()
 {
-  typedef m0cxx0r::Mock<ProductionClass> MockClass;
-  MockClass* mock = MockClass::create();
-  mock->expect("foo", &ProductionClass::foo, 42);
-  mock->expect("bar", &ProductionClass::bar, 42, 43);
-  mock->expect("baz", &ProductionClass::baz);
-  mock->foo(42);
-  mock->bar(3, 4); // bad parameters
-  // missing call to baz
-  mock->verify();
-  MockClass::destroy(&mock);
+    typedef m0cxx0r::Mock<ProductionClass> MockClass;
+    MockClass* mock = MockClass::create();
+    mock->expect("foo", &ProductionClass::foo, 42);
+    mock->expect("bar", &ProductionClass::bar, 42, 43);
+    mock->expect("baz", &ProductionClass::baz);
+    mock->foo(42);
+    mock->bar(3, 4); // bad parameters
+    // missing call to baz
+    mock->verify();
+    MockClass::destroy(&mock);
 }
